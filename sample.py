c2257574dcd7cb1335d09e6de97229de9de9a8b1
WU MENGHUNG <wu.menghung@jp.panasonic.com>
2023-06-06 14:16:28 +0900

    use new datetime

--- sample.py
+++ sample.py
@@ -3,6 +3,7 @@ import time
 import datetime
 from influxdb import InfluxDBClient
 
+# args are host, port, username, password, dbname
 client = InfluxDBClient('127.0.0.1', 8086, 'root', 'root', 'sample')
 
 # databaseの存在を判定し、作成前であれば新規作成
@@ -11,8 +12,8 @@ sample_db = {'name': 'sample'}
 if sample_db not in dbs:
     client.create_database('sample')
 
-t_delta = datetime.timedelta(hours=9)
-JST = datetime.timezone(t_delta, 'JST')
+# t_delta = datetime.timedelta(hours=9)
+# JST = datetime.timezone(t_delta, 'JST')
 
 data_array = []
 while True:
@@ -20,9 +21,8 @@ while True:
         cpu_temperature = int(t.read()) / 1000
 
     print(f'temp {cpu_temperature}')
-    # now = datetime.datetime.now(JST)
-    now = datetime.datetime.now(datetime.timezone.utc)
-    timestamp = now.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    timestamp = datetime.datetime.fromtimestamp(time.time(), datetime.timezone.utc).isoformat('T')
+
     print(f'{timestamp}')
     # "time": "2009-11-10T23:00:00Z",
     # インポートするjsonデータを作成
@@ -38,25 +38,9 @@ while True:
         "measurement": "metrics",
         "time": timestamp
     })
-    # import_array = [
-    # {
-    #   "fields" : {
-    #     "cpu" : cpu_temperature,
-    #     "mem" : 20.0,
-    #   },
-    #   "tags" : {
-    #     "category" : "fuga",
-    #     "machine" : "web02"
-    #   },
-    #   "measurement" : "metrics"
-    #   #"time" : timestamp
-    # }
-    # ]
-    # client.write_points(import_array)
     # データ投入
     if len(data_array) > 5:
         client.write_points(data_array)
         data_array.clear()
-
 
     time.sleep(1)
